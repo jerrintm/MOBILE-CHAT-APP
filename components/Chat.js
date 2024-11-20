3b1a5276a2a11c878730f7661a19a4d36a0469b5
jerrintm <jerrintm@gmail.com>
2024-11-19 23:16:56 -0500

    Updates for task 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,15 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
 
 const Chat = ({ route, navigation }) => {
    const { name } = route.params; 
+   const [messages, setMessages] = useState([]);
+
+    //sets name as name typed by user on mainscreen and sets background as user selected background
+  useEffect(() => {
+    navigation.setOptions({ title: name, color: background });
+  }, []);
 
    useEffect(() => {
         navigation.setOptions({ title: name });
@@ -10,6 +17,20 @@ const Chat = ({ route, navigation }) => {
  
  return (
    <View style={[styles.container, { backgroundColor: background }]}>
+     <GiftedChat
+            messages={messages}
+            renderBubble={renderBubble}
+            renderInputToolbar={renderInputToolbar}
+            renderActions={renderCustomActions}
+            renderCustomView={renderCustomView}
+            onSend={(messages) => onSend(messages)}
+            user={{
+              _id: userID,
+              name: username,
+            }}
+          />
+          { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+          {Platform.OS === "ios"?<KeyboardAvoidingView behavior="padding" />: null}
      <Text>Hello Screen2!</Text>
    </View>
  );
